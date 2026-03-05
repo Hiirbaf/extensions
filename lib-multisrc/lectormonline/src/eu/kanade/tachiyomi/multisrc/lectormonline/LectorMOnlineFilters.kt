fca14a2782006bd4d2cd897445c352bc4d8d7b5f
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-03-04 21:25:04 -0300

    Refactor filters and expand genre options
    Refactor SortByFilter and GenreFilter classes for clarity and add more genre options.

--- lib-multisrc/lectormonline/src/eu/kanade/tachiyomi/multisrc/lectormonline/LectorMOnlineFilters.kt
+++ lib-multisrc/lectormonline/src/eu/kanade/tachiyomi/multisrc/lectormonline/LectorMOnlineFilters.kt
@@ -2,12 +2,15 @@ package eu.kanade.tachiyomi.multisrc.lectormonline
 
 import eu.kanade.tachiyomi.source.model.Filter
 
-class SortByFilter(title: String, private val sortProperties: List<SortProperty>, defaultIndex: Int) :
-    Filter.Sort(
-        title,
-        sortProperties.map { it.name }.toTypedArray(),
-        Selection(defaultIndex, ascending = false),
-    ) {
+class SortByFilter(
+    title: String,
+    private val sortProperties: List<SortProperty>,
+    defaultIndex: Int,
+) : Filter.Sort(
+    title,
+    sortProperties.map { it.name }.toTypedArray(),
+    Selection(defaultIndex, ascending = false),
+) {
     val selected: String
         get() = sortProperties[state!!.index].value
 }
@@ -16,15 +19,44 @@ class SortProperty(val name: String, val value: String) {
     override fun toString(): String = name
 }
 
-class GenreFilter(genres: List<Pair<String, String>>) :
-    UriPartFilter(
-        "Género",
-        arrayOf(
-            Pair("Todos", ""),
-            *genres.toTypedArray(),
-        ),
+/**
+ * Aquí enviamos exactamente el mismo texto
+ * que la API acepta (con mayúsculas).
+ */
+class GenreFilter : UriPartFilter(
+    "Género",
+    arrayOf(
+        "Todos" to "",
+        "Nuevo" to "Nuevo",
+        "Acción" to "Acción",
+        "Fantasía" to "Fantasía",
+        "Sistema" to "Sistema",
+        "Murim" to "Murim",
+        "Romance" to "Romance",
+        "Comedia" to "Comedia",
+        "Drama" to "Drama",
+        "Isekai" to "Isekai",
+        "Reencarnación" to "Reencarnación",
+        "Regresion" to "Regresion",
+        "Retornado" to "Retornado",
+        "Demonios" to "Demonios",
+        "Harem" to "Harem",
+        "Ecchi" to "Ecchi",
+        "Seinen " to "Seinen ",
+        "Shounen " to "Shounen ",
+        "Shoujo" to "Shoujo",
+        "Manhwa" to "Manhwa",
+        "Manhua" to "Manhua",
+        "Webtoon" to "Webtoon",
     )
+)
 
-open class UriPartFilter(displayName: String, private val vals: Array<Pair<String, String>>) : Filter.Select<String>(displayName, vals.map { it.first }.toTypedArray()) {
-    fun toUriPart() = vals[state].second
+open class UriPartFilter(
+    displayName: String,
+    private val vals: Array<Pair<String, String>>,
+) : Filter.Select<String>(
+    displayName,
+    vals.map { it.first }.toTypedArray(),
+) {
+    fun toUriPart(): String = vals[state].second
 }
