a3ace5c78facd86d36c82c018f5c9ce8c07a6f78
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-03-06 10:35:35 -0300

    Update LectorMOnline.kt

--- lib-multisrc/lectormonline/src/eu/kanade/tachiyomi/multisrc/lectormonline/LectorMOnline.kt
+++ lib-multisrc/lectormonline/src/eu/kanade/tachiyomi/multisrc/lectormonline/LectorMOnline.kt
@@ -1,7 +1,6 @@
 package eu.kanade.tachiyomi.multisrc.lectormonline
 
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
@@ -32,7 +31,7 @@ open class LectorMOnline(
 
         return MangasPage(
             mangas,
-            false
+            false,
         )
     }
 
