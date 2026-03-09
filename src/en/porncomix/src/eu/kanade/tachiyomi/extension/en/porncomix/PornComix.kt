f664beeb8c96bebac4a9c0397645f1ce7c855f3d
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-03-09 05:22:27 -0300

    Update PornComix.kt

--- src/en/porncomix/src/eu/kanade/tachiyomi/extension/en/porncomix/PornComix.kt
+++ src/en/porncomix/src/eu/kanade/tachiyomi/extension/en/porncomix/PornComix.kt
@@ -6,6 +6,7 @@ import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.model.UpdateStrategy
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
