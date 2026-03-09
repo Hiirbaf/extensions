bca97504c7669c4f8ba893db74e1314bf5b19a9c
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-03-09 05:01:45 -0300

    Update PornComix.kt

--- src/en/porncomix/src/eu/kanade/tachiyomi/extension/en/porncomix/PornComix.kt
+++ src/en/porncomix/src/eu/kanade/tachiyomi/extension/en/porncomix/PornComix.kt
@@ -3,16 +3,13 @@ package eu.kanade.tachiyomi.extension.en.porncomix
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
-import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
-import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.util.concurrent.TimeUnit
