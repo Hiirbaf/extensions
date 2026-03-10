71b1d3da80ad6c9f7f527774e8dc7c6b71a133fb
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-03-10 01:25:36 -0300

    Update ManhwaLatino.kt

--- src/es/manhwalatino/src/eu/kanade/tachiyomi/extension/es/manhwalatino/ManhwaLatino.kt
+++ src/es/manhwalatino/src/eu/kanade/tachiyomi/extension/es/manhwalatino/ManhwaLatino.kt
@@ -17,6 +17,16 @@ class ManhwaLatino :
 
     override val client: OkHttpClient = network.cloudflareClient.newBuilder()
         .rateLimitHost(baseUrl.toHttpUrl(), 1, 2)
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .header(
+                    "User-Agent",
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122 Safari/537.36"
+                )
+                .header("Referer", baseUrl)
+                .build()
+            chain.proceed(request)
+        }
         .build()
 
     override val useNewChapterEndpoint = false
