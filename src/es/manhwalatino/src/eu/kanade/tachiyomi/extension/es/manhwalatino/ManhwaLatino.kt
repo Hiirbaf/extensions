119aa51f1b660e6580dfafff51b88ee73b0d7fa9
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-03-10 07:32:47 -0300

    Update ManhwaLatino.kt

--- src/es/manhwalatino/src/eu/kanade/tachiyomi/extension/es/manhwalatino/ManhwaLatino.kt
+++ src/es/manhwalatino/src/eu/kanade/tachiyomi/extension/es/manhwalatino/ManhwaLatino.kt
@@ -22,7 +22,7 @@ class ManhwaLatino :
         SimpleDateFormat("dd/MM/yyyy", Locale("es")),
     ) {
 
-    override val client: OkHttpClient = super.cloudflareClient.newBuilder()
+    override val client: OkHttpClient = network.cloudflareClient.newBuilder()
         .rateLimitHost(baseUrl.toHttpUrl(), 1, 1)
         .addInterceptor { chain ->
             val request = chain.request()
