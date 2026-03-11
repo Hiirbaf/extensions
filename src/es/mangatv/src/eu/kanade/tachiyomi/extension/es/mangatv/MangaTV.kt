b4f4154755d66e442fa13ecebad4857642268c14
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-03-11 11:54:47 -0300

    Update MangaTV.kt

--- src/es/mangatv/src/eu/kanade/tachiyomi/extension/es/mangatv/MangaTV.kt
+++ src/es/mangatv/src/eu/kanade/tachiyomi/extension/es/mangatv/MangaTV.kt
@@ -1,9 +1,7 @@
 package eu.kanade.tachiyomi.extension.es.mangatv
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import eu.kanade.tachiyomi.source.model.FilterList
-import java.text.SimpleDateFormat
-import java.util.Locale
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
 class MangaTV :
     MangaThemesia(
@@ -11,12 +9,8 @@ class MangaTV :
         "https://mangatv.net",
         "es",
         mangaUrlDirectory = "/lista",
-        dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.ROOT),
     ) {
-    override fun chapterListSelector() = "div.eplister ul li:has(div.chbox):has(div.eph-num):has(a[href])"
-
-    override fun getFilterList(): FilterList {
-        val filters = super.getFilterList().filterNot { it is AuthorFilter || it is YearFilter }
-        return FilterList(filters)
+    override val client = super.client.newBuilder()
+        .rateLimit(3)
+        .build()
     }
-}
