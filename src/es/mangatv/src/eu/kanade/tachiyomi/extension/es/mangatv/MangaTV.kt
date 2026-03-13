9a044d66dd638f77bc51d40b4aae48edb13a5717
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-03-13 04:55:19 -0300

    Update MangaTV.kt

--- src/es/mangatv/src/eu/kanade/tachiyomi/extension/es/mangatv/MangaTV.kt
+++ src/es/mangatv/src/eu/kanade/tachiyomi/extension/es/mangatv/MangaTV.kt
@@ -1,16 +1,124 @@
 package eu.kanade.tachiyomi.extension.es.mangatv
 
+import android.util.Base64
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.Page
+import keiyoushi.lib.unpacker.Unpacker
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Request
+import org.jsoup.nodes.Document
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MangaTV :
     MangaThemesia(
-        "Manga TV",
+        "Manga  TV",
         "https://mangatv.net",
         "es",
         mangaUrlDirectory = "/lista",
+        dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.ROOT),
     ) {
-    override val client = super.client.newBuilder()
-        .rateLimit(3)
-        .build()
+
+    override val seriesDescriptionSelector = "b:contains(Sinopsis) + span"
+
+    override fun pageListParse(document: Document): List<Page> {
+        val unpackedScript = document.selectFirst("script:containsData(eval)")!!.data()
+            .let(Unpacker::unpack)
+
+        val imageListJson = JSON_IMAGE_LIST_REGEX.find(unpackedScript)?.destructured?.toList()?.get(0).orEmpty()
+        val imageList = try {
+            json.parseToJsonElement(imageListJson.replace(TRAILING_COMMA_REGEX, "]")).jsonArray
+        } catch (_: IllegalArgumentException) {
+            emptyList()
+        }
+        return imageList.mapIndexed { i, jsonEl ->
+            val encodedLink = jsonEl.jsonPrimitive.content
+            val decodedLink = String(Base64.decode(encodedLink, Base64.DEFAULT))
+            Page(i, document.location(), "https:$decodedLink")
+        }
+    }
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val url = baseUrl.toHttpUrl().newBuilder()
+            .addPathSegment(mangaUrlDirectory.substring(1))
+            .addQueryParameter("s", query)
+            .addQueryParameter("page", page.toString())
+        return GET(url.build(), headers)
+    }
+
+    // TODO: add demografia, order, tipos, genre
+    override fun getFilterList() = FilterList(
+
+        DemographyFilter(
+            arrayOf(
+                "Todos",
+                "Shounen",
+                "Shoujo",
+                "Seinen",
+                "Josei",
+            ),
+        ),
+
+        TypeFilter(
+            listOf(
+                "Manga",
+                "Manhwa",
+                "Manhua",
+                "Novela",
+                "OEL",
+            ),
+        ),
+
+        GenreFilter(
+            listOf(
+                "Acción",
+                "Aventura",
+                "Comedia",
+                "Drama",
+                "Fantasía",
+                "Romance",
+                "Slice of Life",
+                "Harem",
+                "Isekai",
+                "Ciencia Ficción",
+            ),
+        ),
+
+        OrderFilter(),
+    )
+
+    companion object {
+        val TRAILING_COMMA_REGEX = """,\s+]""".toRegex()
+    }
 }
+
+private class GenreFilter(genres: List<String>) :
+    Filter.Group<Filter.CheckBox>(
+        "Géneros",
+        genres.map { Filter.CheckBox(it) },
+    )
+
+private class TypeFilter(types: List<String>) :
+    Filter.Group<Filter.CheckBox>(
+        "Tipo",
+        types.map { Filter.CheckBox(it) },
+    )
+
+private class DemographyFilter(demographies: Array<String>) : Filter.Select("Demografía", demographies)
+
+private class OrderFilter :
+    Filter.Select(
+        "Ordenar",
+        arrayOf(
+            "Por defecto",
+            "Popularidad",
+            "Actualizado",
+            "Título",
+            "Rating",
+        ),
+    )
