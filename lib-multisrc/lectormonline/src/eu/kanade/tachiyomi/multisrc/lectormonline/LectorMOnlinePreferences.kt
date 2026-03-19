af47336becec59adb1a4e213eb5db1fa6ba8eb6c
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-03-19 18:47:23 -0300

    Create LectorMOnlinePreferences.kt

---
+++ lib-multisrc/lectormonline/src/eu/kanade/tachiyomi/multisrc/lectormonline/LectorMOnlinePreferences.kt
@@ -0,0 +1,29 @@
+package eu.kanade.tachiyomi.multisrc.lectormonline
+
+import android.content.SharedPreferences
+import androidx.preference.CheckBoxPreference
+import androidx.preference.PreferenceScreen
+
+class LectorMOnlinePreferences(
+    private val preferences: SharedPreferences,
+) {
+
+    fun setup(screen: PreferenceScreen) {
+        val nsfwPref = CheckBoxPreference(screen.context).apply {
+            key = SHOW_NSFW
+            title = "Mostrar contenido NSFW"
+            summary = "Incluir mangas +18 en los resultados"
+            setDefaultValue(false)
+        }
+
+        screen.addPreference(nsfwPref)
+    }
+
+    fun showNsfw(): Boolean {
+        return preferences.getBoolean(SHOW_NSFW, false)
+    }
+
+    companion object {
+        const val SHOW_NSFW = "show_nsfw"
+    }
+}
