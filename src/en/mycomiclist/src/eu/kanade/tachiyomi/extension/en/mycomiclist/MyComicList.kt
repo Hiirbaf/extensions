2404beeba76583b54f7af366408dfdc805416d7a
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-03-23 04:42:48 -0300

    Update MyComicList.kt

--- src/en/mycomiclist/src/eu/kanade/tachiyomi/extension/en/mycomiclist/MyComicList.kt
+++ src/en/mycomiclist/src/eu/kanade/tachiyomi/extension/en/mycomiclist/MyComicList.kt
@@ -1,7 +1,12 @@
 package eu.kanade.tachiyomi.extension.en.mycomiclist
 
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.*
+import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import okhttp3.Request
 import org.jsoup.nodes.Document
