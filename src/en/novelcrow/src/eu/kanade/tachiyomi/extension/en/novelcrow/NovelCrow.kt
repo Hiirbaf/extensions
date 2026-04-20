ba718ca6cd29ab39e110a9fa381c9c075514106c
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-04-20 16:43:39 -0300

    Update NovelCrow.kt

--- src/en/novelcrow/src/eu/kanade/tachiyomi/extension/en/novelcrow/NovelCrow.kt
+++ src/en/novelcrow/src/eu/kanade/tachiyomi/extension/en/novelcrow/NovelCrow.kt
@@ -1,7 +1,19 @@
 package eu.kanade.tachiyomi.extension.en.novelcrow
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
+import okhttp3.Request
 
 class NovelCrow : Madara("NovelCrow", "https://novelcrow.com", "en") {
     override val useNewChapterEndpoint = true
+
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/comic/?m_orderby=trending&page=$page", headers)
+
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/comic/?m_orderby=&page=$page", headers)
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val url = "$baseUrl/comic/?s=$query&post_type=wp-manga&page=$page"
+        return GET(url, headers)
+    }
 }
