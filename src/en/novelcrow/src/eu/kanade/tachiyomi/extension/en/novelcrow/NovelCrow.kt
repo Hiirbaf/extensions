9fe50ff873064060601c42839164dd0aa762158c
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-04-20 18:01:24 -0300

    Update NovelCrow.kt

--- src/en/novelcrow/src/eu/kanade/tachiyomi/extension/en/novelcrow/NovelCrow.kt
+++ src/en/novelcrow/src/eu/kanade/tachiyomi/extension/en/novelcrow/NovelCrow.kt
@@ -7,6 +7,9 @@ import okhttp3.Request
 class NovelCrow : Madara("NovelCrow", "https://novelcrow.com", "en") {
     override val useNewChapterEndpoint = true
     override val mangaSubString = "comic"
+    
+    // Fuerza search via URL en lugar de AJAX
+    override val fetchSearchMangaViaAjax = false
 
     override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/comic/?m_orderby=trending&page=$page", headers)
 
