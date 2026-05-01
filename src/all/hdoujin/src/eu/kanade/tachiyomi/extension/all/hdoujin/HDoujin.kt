352b5d5ecf75bcba95f38ab1147e00f78cc5ea08
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-05-01 05:15:12 -0300

    Update HDoujin ID for Korean language support

--- src/all/hdoujin/src/eu/kanade/tachiyomi/extension/all/hdoujin/HDoujin.kt
+++ src/all/hdoujin/src/eu/kanade/tachiyomi/extension/all/hdoujin/HDoujin.kt
@@ -48,7 +48,7 @@ class HDoujin(
     override val name = "HDoujin"
 
     override val id: Long = when (lang) {
-        "ko" -> 1528745830L
+        "ko" -> 9007165605477226632L
         else -> super.id
     }
 
