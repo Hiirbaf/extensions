ea103925eb14c902af2041d7a2f2f532a0d7ffa2
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-05-03 15:32:33 -0300

    Add import for Data class in CapibaraTraductor.kt

--- src/all/capibaratraductor/src/eu/kanade/tachiyomi/extension/all/capibaratraductor/CapibaraTraductor.kt
+++ src/all/capibaratraductor/src/eu/kanade/tachiyomi/extension/all/capibaratraductor/CapibaraTraductor.kt
@@ -1,5 +1,6 @@
 package eu.kanade.tachiyomi.extension.all.capibaratraductor
 
+import eu.kanade.tachiyomi.multisrc.lectormoe.Data
 import eu.kanade.tachiyomi.multisrc.lectormoe.LectorMoe
 import eu.kanade.tachiyomi.multisrc.lectormoe.SeriesListDataDto
 import eu.kanade.tachiyomi.network.GET
