7c3b58f5ed4ec8688ad13e81056f680136e08f12
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-05-02 23:54:41 -0300

    Refactor package declaration and class formatting

--- src/all/capibaratraductor/src/eu/kanade/tachiyomi/extension/all/capibaratraductor/CapibaraTraductor.kt
+++ src/all/capibaratraductor/src/eu/kanade/tachiyomi/extension/all/capibaratraductor/CapibaraTraductor.kt
@@ -1,9 +1,10 @@
-package eu.kanade.tachiyomi.extension.es.capibaratraductor
+package eu.kanade.tachiyomi.extension.all.capibaratraductor
 
 import eu.kanade.tachiyomi.multisrc.lectormoe.LectorMoe
 
-class CapibaraTraductor : LectorMoe(
-    name = "CapibaraTraductor",
-    baseUrl = "https://capibaratraductor.com",
-    lang = "es",
-)
+class CapibaraTraductor :
+    LectorMoe(
+        name = "CapibaraTraductor",
+        baseUrl = "https://capibaratraductor.com",
+        lang = "es",
+    )
